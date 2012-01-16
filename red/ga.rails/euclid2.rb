f6916be1418b7ba7052173c45e7a0cd0572d50af
Kirsten Power <kirsten@workstrand.com>
2012-01-16 13:10:47 -0500

    new commits
    sync new class commits

---
+++ red/ga.rails/euclid2.rb
@@ -0,0 +1,45 @@
+=begin 
+Add a `squared_euclidean_distance` method to the `Hash` class that takes another `Hash` as a parameter and calculates the euclidean distance with another Hash. A Euclidean distance between `{ :x => 1, :y => 2 }` and `{ :x => 3, :y => 4 }` is `(1 - 3)^2 + (2 - 4)^2`. 
+
+    {}.squared_euclidean_distance({}) # 0
+
+    { :x => 1, :y => 2 }.squared_euclidean_distance({ :z => 3 }) # 14
+
+    { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :z => 5 }) # 30
+
+    { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) # 10
+
+    { :x => 10, :y => 20 }.squared_euclidean_distance({}) # 500
+
+    {}.squared_euclidean_distance({ :x => 10, :y => 20 }) # 500 
+=end 
+class Hash 
+  def self.squared_euclidean_distance(lhs, rhs)
+	
+	keys = (lhs.keys + rhs.keys).uniq
+	
+	keys.inject(0) {|a,k| a+((lhs[k] || 0) - (rhs[k] || 0))**2 }
+	
+	#((lhs[:x] || 0) - (rhs[:x] || 0))**2 + ((lhs[:y] || 0) - (rhs[:y] || 0))**2 + ((lhs[:z] || 0) - (rhs[:z] || 0))**2
+	
+  end 
+  def squared_euclidean_distance(rhs)
+
+    Hash.squared_euclidean_distance(self, rhs)
+
+  end 
+end 
+
+raise "invalid distance between two different hashes" unless 14 == ( { :x => 1, :y => 2 }.squared_euclidean_distance({ :z => 3 }) )
+
+raise "invalid distance between empty hashes" unless 0 == ( {}.squared_euclidean_distance({}) )
+
+raise "invalid distance between two overlapping hashes" unless 10 == ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) )
+
+raise "invalid distance between two partially overlapping hashes" unless 30 == ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :z => 5 }) )
+
+raise "invalid distance between a hash and an empty hash" unless 500 == ( { :x => 10, :y => 20 }.squared_euclidean_distance({}) )
+
+raise "invalid distance between an empty hash and a hash" unless 500 == ( {}.squared_euclidean_distance({ :x => 10, :y => 20 }) )
+
+puts "correct results in squared_euclidean_distance"
